a186dab8e7d4de9019d4af6402ddbeaae6c2a2d1
Deok Jang <deok@Deoks-MacBook-Air.local>
2024-03-24 16:03:04 +0900

    Committer: Deok Jang <deok@Deoks-MacBook-Air.local>  On branch main  Initial commit

---
+++ programs/orca-tst/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Auk5McF2qPTR2f1CXxvg8MGPSiPGusULqE5k2sLjJy7g");
+
+#[program]
+pub mod orca_tst {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
