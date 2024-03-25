1077179c8dbc29e4ce006d2807b578f484350b4b
Deok Jang <dead.pool.aka.wilson@gmail.com>
2024-03-25 16:01:02 +0900

    Add orca crate import  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   .gitignore 	modified:   Cargo.lock 	modified:   programs/orca-tst/Cargo.toml 	modified:   programs/orca-tst/src/lib.rs

--- programs/orca-tst/src/lib.rs
+++ programs/orca-tst/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Auk5McF2qPTR2f1CXxvg8MGPSiPGusULqE5k2sLjJy7g");
 
 #[program]
-pub mod orca_tst {
+pub mod orca_test {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
