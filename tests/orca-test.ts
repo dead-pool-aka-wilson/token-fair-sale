d0a27e20398af5b6c61e2d99734b94217eb4b01f
Deok <dead.pool.aka.wilson@gmail.com>
2024-03-31 19:33:25 +0900

    Add decimal.js

--- tests/orca-test.ts
+++ tests/orca-test.ts
@@ -1,5 +1,9 @@
 import * as anchor from '@coral-xyz/anchor';
-import { Program } from '@coral-xyz/anchor';
+import {
+    Program,
+    AnchorProvider,
+    Wallet as AnchorWallet,
+} from '@coral-xyz/anchor';
 import { OrcaTest } from '../target/types/orca_test';
 import {
     PublicKey,
@@ -13,74 +17,51 @@ import {
     ORCA_WHIRLPOOLS_CONFIG,
     PDAUtil,
     PriceMath,
-    SwapUtils,
-    swapQuoteByInputToken,
     WhirlpoolContext,
     buildWhirlpoolClient,
-    increaseLiquidityQuoteByInputToken,
-    decreaseLiquidityQuoteByLiquidity,
     PoolUtil,
-    IGNORE_CACHE,
     TickUtil,
 } from '@orca-so/whirlpools-sdk';
 import { createMint } from '@solana/spl-token';
-import {
-    TOKEN_PROGRAM_ID,
-    AccountLayout,
-    ASSOCIATED_TOKEN_PROGRAM_ID,
-    getAssociatedTokenAddressSync,
-    NATIVE_MINT,
-} from '@solana/spl-token';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import {
     TransactionBuilder,
-    resolveOrCreateATA,
-    DecimalUtil,
-    Percentage,
     Wallet,
     TransactionBuilderOptions,
 } from '@orca-so/common-sdk';
-import { assert, expect } from 'chai';
+import { assert } from 'chai';
 import BN from 'bn.js';
 
 const SOL = {
     mint: new PublicKey('So11111111111111111111111111111111111111112'),
     decimals: 9,
 };
-const SAMO = {
-    mint: new PublicKey('7xKXtg2CW87d97TXJSDpbD5jBkheTqA83TZRuJosgAsU'),
-    decimals: 9,
-};
-const USDC = {
-    mint: new PublicKey('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
-    decimals: 6,
-};
-
+const TEST_PROVIDER_URL = 'http://localhost:8899';
 const TEST_WALLET_SECRET = [
-    1, 62, 224, 23, 71, 125, 243, 82, 250, 21, 113, 130, 171, 89, 11, 153, 89,
-    4, 47, 101, 182, 60, 144, 109, 181, 163, 145, 150, 102, 78, 156, 129, 167,
-    207, 70, 158, 113, 250, 28, 163, 215, 77, 43, 135, 192, 208, 172, 76, 136,
-    77, 108, 26, 82, 55, 38, 90, 21, 205, 196, 83, 235, 255, 162, 122,
+    171, 47, 220, 229, 16, 25, 41, 67, 249, 72, 87, 200, 99, 166, 155, 51, 227,
+    166, 151, 173, 73, 247, 62, 43, 121, 185, 218, 247, 54, 154, 12, 174, 176,
+    136, 16, 247, 145, 71, 131, 112, 92, 104, 49, 155, 204, 211, 96, 225, 184,
+    95, 61, 41, 136, 83, 9, 18, 137, 122, 214, 38, 247, 37, 158, 162,
 ];
 
 describe('orca-test', () => {
-    // Configure the client to use the local cluster.
-    const provider = anchor.AnchorProvider.env();
-    const connection = provider.connection;
-    const wallet = provider.wallet as Wallet;
+    const connection = new Connection(TEST_PROVIDER_URL, 'confirmed');
     const testWallet = Keypair.fromSecretKey(
         new Uint8Array(TEST_WALLET_SECRET),
     );
-
-    anchor.setProvider(provider);
-
     const program = anchor.workspace.OrcaTest as Program<OrcaTest>;
+    const provider = new AnchorProvider(
+        connection,
+        new AnchorWallet(testWallet),
+        { commitment: 'confirmed' },
+    );
+    const wallet = provider.wallet as Wallet;
 
     const whirlpool_ctx = WhirlpoolContext.withProvider(
         provider,
         ORCA_WHIRLPOOL_PROGRAM_ID,
     );
     const fetcher = whirlpool_ctx.fetcher;
-    const whirlpool_client = buildWhirlpoolClient(whirlpool_ctx);
 
     const transaction_builder_opts: TransactionBuilderOptions = {
         defaultBuildOption: {
@@ -93,92 +74,54 @@ describe('orca-test', () => {
         },
     };
 
-    const sol_usdc_whirlpool_pubkey = PDAUtil.getWhirlpool(
-        ORCA_WHIRLPOOL_PROGRAM_ID,
-        ORCA_WHIRLPOOLS_CONFIG,
-        SOL.mint,
-        USDC.mint,
-        64,
-    ).publicKey;
-    const samo_usdc_whirlpool_pubkey = PDAUtil.getWhirlpool(
-        ORCA_WHIRLPOOL_PROGRAM_ID,
-        ORCA_WHIRLPOOLS_CONFIG,
-        SAMO.mint,
-        USDC.mint,
-        64,
-    ).publicKey;
-
-    const position_mint_keypair = Keypair.generate();
-
-    const position_mint = position_mint_keypair.publicKey;
-    const position_pda = PDAUtil.getPosition(
-        ORCA_WHIRLPOOL_PROGRAM_ID,
-        position_mint,
-    );
-
-    const verify_log = (logs: string[], message: string) => {
-        expect(logs).includes(`Program log: verify! ${message}`);
-    };
-    const rent_ta = async () => {
-        return connection.getMinimumBalanceForRentExemption(AccountLayout.span);
-    };
-    const sleep = second =>
-        new Promise(resolve => setTimeout(resolve, second * 1000));
-
     it('execute proxy initialize_pool and initialize_tick_array', async () => {
-        let NEW_TOKEN_MINT: PublicKey;
-        while (!NEW_TOKEN_MINT) {
+        let NEW_SAMO_MINT: PublicKey;
+        while (!NEW_SAMO_MINT) {
             const mint = await createMint(
                 connection,
                 testWallet,
                 wallet.publicKey,
                 wallet.publicKey,
                 9,
-                undefined,
-                { skipPreflight: true },
             );
-
             const [mint_a, mint_b] = PoolUtil.orderMints(mint, SOL.mint);
-
             if (mint_a.toString() === mint.toString()) {
-                NEW_TOKEN_MINT = mint;
+                NEW_SAMO_MINT = mint;
             }
         }
-        console.log('NEW TOKEN MINT : ', NEW_TOKEN_MINT.toString());
 
         const tick_spacing = 128;
-
         const fee_tier_128_pubkey = PDAUtil.getFeeTier(
             ORCA_WHIRLPOOL_PROGRAM_ID,
             ORCA_WHIRLPOOLS_CONFIG,
             tick_spacing,
         ).publicKey;
 
-        const new_token_sol_whirlpool_ts_128_pubkey = PDAUtil.getWhirlpool(
+        const new_samo_usdc_whirlpool_ts_128_pubkey = PDAUtil.getWhirlpool(
             ORCA_WHIRLPOOL_PROGRAM_ID,
             ORCA_WHIRLPOOLS_CONFIG,
-            NEW_TOKEN_MINT,
+            NEW_SAMO_MINT,
             SOL.mint,
             tick_spacing,
         ).publicKey;
 
         // use SAMO/USDC (ts=64) whirlpool price as initial sqrt price
-        const initial_sqrt_price = new BN('10000000000000000');
+        const initial_sqrt_price = samo_usdc_whirlpool.sqrtPrice;
 
-        const new_token_vault_keypair = Keypair.generate();
-        const sol_vault_keypair = Keypair.generate();
+        const new_samo_vault_keypair = Keypair.generate();
+        const usdc_vault_keypair = Keypair.generate();
 
         const initialize_pool = await program.methods
             .proxyInitializePool(tick_spacing, initial_sqrt_price)
             .accounts({
                 whirlpoolProgram: ORCA_WHIRLPOOL_PROGRAM_ID,
                 whirlpoolsConfig: ORCA_WHIRLPOOLS_CONFIG,
-                tokenMintA: NEW_TOKEN_MINT,
+                tokenMintA: NEW_SAMO_MINT,
                 tokenMintB: USDC.mint,
                 funder: wallet.publicKey,
-                whirlpool: new_token_sol_whirlpool_ts_128_pubkey,
-                tokenVaultA: new_token_vault_keypair.publicKey,
-                tokenVaultB: sol_vault_keypair.publicKey,
+                whirlpool: new_samo_usdc_whirlpool_ts_128_pubkey,
+                tokenVaultA: new_samo_vault_keypair.publicKey,
+                tokenVaultB: usdc_vault_keypair.publicKey,
                 feeTier: fee_tier_128_pubkey,
                 tokenProgram: TOKEN_PROGRAM_ID,
                 systemProgram: SystemProgram.programId,
@@ -222,11 +165,11 @@ describe('orca-test', () => {
                     .proxyInitializeTickArray(start_tick_index)
                     .accounts({
                         whirlpoolProgram: ORCA_WHIRLPOOL_PROGRAM_ID,
-                        whirlpool: new_token_sol_whirlpool_ts_128_pubkey,
+                        whirlpool: new_samo_usdc_whirlpool_ts_128_pubkey,
                         funder: wallet.publicKey,
                         tickArray: PDAUtil.getTickArray(
                             ORCA_WHIRLPOOL_PROGRAM_ID,
-                            new_token_sol_whirlpool_ts_128_pubkey,
+                            new_samo_usdc_whirlpool_ts_128_pubkey,
                             start_tick_index,
                         ).publicKey,
                         systemProgram: SystemProgram.programId,
@@ -243,7 +186,7 @@ describe('orca-test', () => {
             .addInstruction({
                 instructions: [initialize_pool],
                 cleanupInstructions: [],
-                signers: [new_token_vault_keypair, sol_vault_keypair],
+                signers: [new_samo_vault_keypair, usdc_vault_keypair],
             })
             .addInstruction({
                 instructions: initialize_tick_arrays,
@@ -251,20 +194,14 @@ describe('orca-test', () => {
                 signers: [],
             });
 
-        console.log('1');
-
-        const signature = await transaction.buildAndExecute(undefined, {
-            skipPreflight: true,
-        });
+        const signature = await transaction.buildAndExecute();
         await connection.confirmTransaction(signature);
 
         // verification
         const new_samo_usdc_whirlpool_ts_128 = await fetcher.getPool(
-            new_token_sol_whirlpool_ts_128_pubkey,
+            new_samo_usdc_whirlpool_ts_128_pubkey,
         );
-        assert(
-            new_samo_usdc_whirlpool_ts_128.tokenMintA.equals(NEW_TOKEN_MINT),
-        );
+        assert(new_samo_usdc_whirlpool_ts_128.tokenMintA.equals(NEW_SAMO_MINT));
         assert(new_samo_usdc_whirlpool_ts_128.tokenMintB.equals(USDC.mint));
         assert(new_samo_usdc_whirlpool_ts_128.tickSpacing === tick_spacing);
         assert(new_samo_usdc_whirlpool_ts_128.sqrtPrice.eq(initial_sqrt_price));
@@ -272,7 +209,7 @@ describe('orca-test', () => {
         const tickarray_pubkeys = start_tick_indexes.map(start_tick_index => {
             return PDAUtil.getTickArray(
                 ORCA_WHIRLPOOL_PROGRAM_ID,
-                new_token_sol_whirlpool_ts_128_pubkey,
+                new_samo_usdc_whirlpool_ts_128_pubkey,
                 start_tick_index,
             ).publicKey;
         });
@@ -284,7 +221,7 @@ describe('orca-test', () => {
         tickarrays.forEach((tickarray, i) => {
             assert(
                 tickarray.whirlpool.equals(
-                    new_token_sol_whirlpool_ts_128_pubkey,
+                    new_samo_usdc_whirlpool_ts_128_pubkey,
                 ),
             );
             assert(tickarray.startTickIndex === start_tick_indexes[i]);
